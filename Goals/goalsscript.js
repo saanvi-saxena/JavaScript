7cb7d87e310d571d46eea57af5cdd2fe56f684ad
2025B01010686 <saanvi.25b01010686@abes.ac.in>
2026-07-15 15:52:09 +0530

    Complete CRUD functionality with Local Storage

--- Goals/goalsscript.js
+++ Goals/goalsscript.js
@@ -1,6 +1,100 @@
 const goalInput = document.getElementById("goalInput");
 const addGoalBtn = document.getElementById("addGoalBtn");
 const goalList = document.getElementById("goalList");
+const progressText = document.querySelector("#progressText");
+const progressLevel = document.querySelector(".progressLevel");
+
+const goals = [];
+
+function saveGoals(){
+    localStorage.setItem("goals", JSON.stringify(goals));
+}
+
+function updateProgress(){
+    const checkboxes = document.querySelectorAll('#goalList input[type="checkbox"]');
+    let completedGoals = 0;
+    checkboxes.forEach(function(checkbox){
+        if(checkbox.checked){
+            completedGoals++;
+        }
+    });
+
+    let percentage;
+    if(checkboxes.length === 0){
+        percentage = 0;
+    }
+    else{
+        percentage = Math.round((completedGoals/checkboxes.length)*100);
+    }
+
+    progressText.textContent = completedGoals + "/" + checkboxes.length + " Completed (" + percentage + "%)";
+    progressLevel.style.width = percentage + "%";
+}
+
+function createGoal(goal){
+    const li = document.createElement("li");
+
+        const input = document.createElement("input");
+        input.type = "checkbox";
+        
+        const span = document.createElement("span");
+        span.textContent = goal.text;
+
+        input.checked = goal.completed;
+
+        if(goal.completed){
+            span.classList.add("completed");
+        }
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+
+        li.append(input);
+        li.append(span);
+        li.append(editBtn);
+        li.append(deleteBtn);
+
+        goalList.append(li);
+        
+
+        //goalInput.value = ""; delete
+
+        editBtn.addEventListener("click", function(){
+            const newGoal = prompt("Edit your goal." , goal.text);
+            if (newGoal !== null && newGoal.trim() != ""){
+                goal.text = newGoal.trim();
+                span.textContent = newGoal.trim();
+                saveGoals();
+            }
+        });
+
+        deleteBtn.addEventListener("click", function(){
+            const index = goals.indexOf(goal);
+            if(index !== -1){
+                goals.splice(index, 1);
+            }
+            saveGoals();
+            li.remove();
+            updateProgress();
+        });
+
+        input.addEventListener("change", function(){
+            if(input.checked){
+                span.classList.add("completed");
+                goal.completed = true;
+            }
+            else{
+                span.classList.remove("completed");
+                goal.completed = false;
+            }
+
+            saveGoals();
+            updateProgress();
+        });
+};
 
 addGoalBtn.addEventListener("click", function(){
     if(goalInput.value === ""){
@@ -8,38 +102,17 @@ addGoalBtn.addEventListener("click", function(){
     }
     else{
         //console.log(goalInput.value);
-        const li = document.createElement("li");
+        goals.push({
+            text: goalInput.value,
+            completed: false
+        });
 
-        const input = document.createElement("input");
-        input.type = "checkbox";
-        
-        const span = document.createElement("span");
-        span.textContent = goalInput.value;
-        
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete";
+        saveGoals();
+        //console.log(goals);
 
-        li.append(input);
-        li.append(span);
-        li.append(deleteBtn);
-
-        goalList.append(li);
-
+        createGoal(goals[goals.length-1]);
         goalInput.value = "";
-
-        deleteBtn.addEventListener("click", function(){
-            li.remove();
-        })
-
-        input.addEventListener("change", function(){
-            if(input.checked){
-                span.classList.add("completed");
-            }
-            else{
-                span.classList.remove("completed");
-            }
-        })
-        
+        updateProgress();
     }
 });
 
@@ -48,3 +121,13 @@ goalInput.addEventListener("keydown", function(event){
         addGoalBtn.click();
     }
 });
+
+const savedGoals = JSON.parse(localStorage.getItem("goals")) || [];
+
+savedGoals.forEach(function(goal){
+    goals.push(goal);
+    createGoal(goal);
+});
+
+updateProgress();
+
